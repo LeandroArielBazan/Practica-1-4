d8d942150623fd8caaa2e287e5113e05d84327ba
Leandro Ariel Bazan <leanbazan99@gmail.com>
2023-03-28 11:02:34 -0300

    practica 1-4

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+document.querySelector("body").innerHTML = `
+<details></details>
+        <summary>titulo de info importante</summary>
+         <img src="https://thumbs.gfycat.com/OblongUntriedCow-size_restricted.gif" alt="">
+         <iframe width="600px" height="300px" src="https://es.wikipedia.org/wiki/Wikipedia" frameborder="0"></iframe>
+
+         <p>Cuando Paul, de 17 años, se une al frente occidental en la Primera Guerra Mundial, su entusiasmo se derrumba rápidamente ante la dura realidad de la vida en las trincheras.
+            
+            <a href="https://www.netflix.com/ar/title/81260280?source=35"> Ver pelicula</a>
+` ;
